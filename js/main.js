042836758cd422be9873a2965f34f03f3b6bf4ed
tjr <2871779035@qq.com>
2019-05-28 10:45:36 +0800

    创建了白纸，添加markdown

--- js/main.js
+++ js/main.js
@@ -1,80 +1,137 @@
-var result = `
+var css1 = `
 /*
-    面试官，你好，我是唐静如
+    面试官，你好，我是唐静如，
     只用文字作自我介绍太单调了，
     我用代码来做自我介绍吧，
     首先准备一些样式
 */
-    *{transition:all 1.5s}
+    *{transition:all 1s}
     html{
         background-color:#eee;
-        height:100%;
     }
     #code{
-        border:1px solid black;
+        border:1px solid #aaa;
         padding:16px;
         font-size:16px;
     }
 /*下面我想让代码高亮*/
-    .token.selector{
-        color:#690;
-    }
-    .token.punctuation{
-        color:#999;
-    }
-    .token.property{
-        color:#905;
-    }
-    .token.comment{
-        color: slategray;
-    }
+    .token.selector{color:#690;}
+    .token.punctuation{color:#999;}
+    .token.property{color:#905;}
+    .token.comment{color: slategray;}
 /*加点3D效果*/
     .wrapper{
         transform:rotate(360deg)
     }
 `
-let n = 0
-var wrapper = document.querySelector('.wrapper')
-var id = setInterval(()=>{
-    n += 1
-    code.innerHTML = result.substring(0,n)
-    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css');
-    styleTag.innerHTML = result.substring(0,n)
-    code.scrollTop = 10000
-    if(n >= result.length){
-        window.clearInterval(id)
-        fn2()
-        fn3(result)
+var css2 = `
+/*接下来我需要一张纸*/
+    
+    .code-wrapper{
+        box-shadow:0 0 10px rgba(0,0,0,0.25);
+        width:50%;
+        position:fixed;
+        left:0;
     }
-},10)
-function fn2(){
+    .paper-wrapper{
+        position:fixed;
+        right:0;
+        width:50%;
+        background-color:#aaa;
+    }
+    .wrapper{display:flex;}
+    .paper-wrapper .content{
+        background-color:white;
+    }
+`
+
+var md = `
+#自我介绍
+
+我叫唐静如
+1995年5月出生
+武汉理工大学毕业
+自学前端半年
+希望应聘前端开发岗位
+
+#技能介绍
+
+熟悉HTML CSS JavaScript  
+
+#项目介绍
+
+1、苹果风格轮播
+2、自我介绍简历
+3、自制画板
+
+#联系方式
+
+QQ xxxxxxxxxxxxxxxx
+Email xxxxxxxxxxxxxxxxxxx
+手机 xxxxxxxxxxxxx
+
+#联系方式
+
+QQ xxxxxxxxxxxxxxxx
+Email xxxxxxxxxxxxxxxxxxx
+手机 xxxxxxxxxxxxx
+
+#联系方式
+
+QQ xxxxxxxxxxxxxxxx
+Email xxxxxxxxxxxxxxxxxxx
+手机 xxxxxxxxxxxxx
+
+#联系方式
+
+QQ xxxxxxxxxxxxxxxx
+Email xxxxxxxxxxxxxxxxxxx
+手机 xxxxxxxxxxxxx
+`
+var wrapper = document.querySelector('.wrapper')
+
+writeCode('',css1,()=>{
+    createPaper(()=>{
+        writeCode(css1,css2,()=>{
+            writeMarkdown(md)
+        })
+    })
+})
+function writeCode(prefix,code,fn){
+    let  n = 0
+    let domCode = document.querySelector('#code')
+    var clock = setInterval(()=>{
+        n+=1
+        domCode.innerHTML = Prism.highlight(prefix + code.substring(0,n), Prism.languages.css, 'css')
+        styleTag.innerHTML = prefix + code.substring(0,n)
+        domCode.scrollTop = domCode.scrollHeight
+        if(n >= code.length){
+            window.clearInterval(clock)
+            fn && fn.call()
+        }
+    },30)
+}
+
+function writeMarkdown(markdown,fn){
+    let  n = 0
+    let domMarkdown = document.querySelector('.content')
+    var clock = setInterval(()=>{
+        n+=1
+        domMarkdown.innerHTML = markdown.substring(0,n)
+        domMarkdown.scrollTop = domMarkdown.scrollHeight
+        if(n >= markdown.length){
+            window.clearInterval(clock)
+            fn && fn.call()
+        }
+    },50)
+}
+
+function createPaper(fn){
     var paper = document.createElement('div')
-    paper.className = 'paper'
+    paper.className = 'paper-wrapper'
     wrapper.appendChild(paper)
     var content = document.createElement('pre')
     content.className = 'content'
     paper.appendChild(content)
+    fn && fn.call()
 }
-function fn3(preResult){
-    var result2 = `
-/*接下来我需要一张纸*/
-    .paper{
-        height:100px;
-        width:100px;
-        background:red;
-    }
-    `
-    let n = 0;
-    var clock1 = setInterval(()=>{
-        n += 1
-        code.innerHTML = preResult + result2.substring(0,n)
-        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css');
-        styleTag.innerHTML = preResult + result2.substring(0,n)
-        code.scrollTop = 10000
-        if(n >= result2.length){
-            window.clearInterval(clock1)
-        }
-    },10)
-}
-
-
